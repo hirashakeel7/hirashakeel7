d05823d009b8dfea151a1983a8841a3d342a0b60
hirashakeel7 <104651276+hirashakeel7@users.noreply.github.com>
2022-05-08 12:08:22 -0800

    1st program

---
+++ DataPractice.java
@@ -0,0 +1,14 @@
+public class DataPractice
+{
+ public static void main(String arg[])
+{
+ int n=5;
+ char c='A';
+ float a=1.5f;
+ String userName= "Hira";
+ System.out.println(n);
+ System.out.println(c);
+ System.out.println(a);
+ System.out.println(userName);
+}
+}
